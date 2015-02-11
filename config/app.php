b9780ebc9a5a608209087a8badb9115746900764
Michal Hojgr <michal.hojgr@gmail.com>
2015-02-11 04:17:39 +0100

    GENERAL: Change DB settings, add ide helper
    Temporarily I set DB to use root: connection just to prototype; soon there will be local config

--- config/app.php
+++ config/app.php
@@ -145,6 +145,7 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 
+		'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider', // generates proxy objects for IDE auto competition
 	],
 
 	/*
