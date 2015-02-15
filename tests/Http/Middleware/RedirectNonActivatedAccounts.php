b91b24f9e4b09f88d1f28a2b8a07ffeef3d5326a
Michal Hojgr <michal.hojgr@gmail.com>
2015-02-15 13:43:31 +0100

    BetaRedirect page: add test for authenticated not active user

---
+++ tests/Http/Middleware/RedirectNonActivatedAccounts.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace tests\Http\Middleware;
+
+
+use BookieGG\Models\User;
+
+class RedirectNonActivatedAccounts extends \TestCase {
+    public function testNotAuthed_home_page() {
+        $response = $this->call('GET', route("home"));
+        $this->assertTrue($response->isRedirect(route("beta_home")));
+    }
+
+    public function testNotActivated_home_page() {
+        $test_user = new User();
+        $test_user->active = false;
+
+        \Auth::shouldReceive('user')->twice()->andReturn($test_user);
+
+        $response = $this->call('GET', route('home'));
+        $this->assertTrue($response->isRedirect(route("beta_home")));
+    }
+
+    public function testActive_home_page() {
+        $test_user = new User();
+        $test_user->active = true;
+
+        \Auth::shouldReceive('user')->twice()->andReturn($test_user);
+
+        $response = $this->call('GET', route("home"));
+        $this->assertFalse($response->isRedirection());
+    }
+}
