382413ede3f86f08daf2d01861d6dac1d955a6df
Robert Danielson <rob@seligtech.com>
2015-02-16 11:11:10 -0500

    mv site.css app.css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bookie.GG - Upcoming Matches</title>
 
-    <link href="{{ elixir('css/site.css') }}" rel="stylesheet">
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,100,900' rel='stylesheet' type='text/css'>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
