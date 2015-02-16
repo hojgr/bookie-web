ee7183a864ffe5ba777b9a77ec2ce85f44a0a058
Michal Hojgr <michal.hojgr@gmail.com>
2015-02-16 15:52:31 +0100

    Migration: set default values of string in beta subscriptions

---
+++ database/migrations/2015_02_16_144927_beta_subscriptions_set_varchar_default_null.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BetaSubscriptionsSetVarcharDefaultNull extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('beta_subscriptions', function(Blueprint $table) {
+            $table->dropColumn(['name', 'email']);
+        });
+
+        Schema::table('beta_subscriptions', function(Blueprint $table) {
+            $table->string("name")->after("id")->nullable();
+            $table->string("email")->after("name")->nullable();
+
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
