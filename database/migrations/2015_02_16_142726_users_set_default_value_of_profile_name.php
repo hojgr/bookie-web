c9500a3f49f3698ecd13fdf2a0313a0e995c6850
Michal Hojgr <michal.hojgr@gmail.com>
2015-02-16 15:32:38 +0100

    Migration: set default key for profile_name

---
+++ database/migrations/2015_02_16_142726_users_set_default_value_of_profile_name.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsersSetDefaultValueOfProfileName extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->dropColumn('profile_name');
+        });
+
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->string("profile_name")->default("");
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
