883fa76614c7e822511b3a5fdbcec5fcaffc8a2d
Robert Danielson <rob@seligtech.com>
2015-02-16 10:54:07 -0500

    database: specify mysql socket location from env

--- config/database.php
+++ config/database.php
@@ -54,6 +54,7 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
+            'unix_socket'   => env('MYSQL_SOCK', null),
 			'host'      => env('DB_HOST', 'localhost'),
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
