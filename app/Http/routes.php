7780da48b3c5bf7f44968268cf2eb8c8286b313a
Michal Hojgr <michal.hojgr@gmail.com>
2015-02-16 16:10:37 +0100

    auth: make login pages accessible for everyone

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,13 +16,12 @@ Route::group(['middleware' => 'beta.redirect_not_activated'], function() {
 });
 
 Route::group(['middleware' => 'beta.redirect_activated'], function() {
-    Route::get('/login', ['as' => 'login', 'uses' => 'SteamController@login']);
-    Route::get('/auth', ['as' => 'auth', 'uses' => 'SteamController@auth']);
-
     Route::get('/beta', ['as' => 'beta_home', 'uses' => 'BetaController@index']);
     Route::post('/subscribe', ['as' => 'subscribe', 'uses' => 'BetaSubscribeController@index']);
 
 });
 
+Route::get('/login', ['as' => 'login', 'uses' => 'SteamController@login']);
+Route::get('/auth', ['as' => 'auth', 'uses' => 'SteamController@auth']);
 Route::get('/site', ['as' => 'site_home', 'uses' => 'MatchController@matches']);
 Route::get('/logout', ['as' => 'logout', 'uses' => 'SteamController@logout']);
