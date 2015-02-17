0296f5029f780b43613d830447be4cc3a2f777bb
robdan <rob@bigkesh.com>
2015-02-16 22:39:12 -0500

    accidentally added a second content container to live match template

--- resources/views/home/live.blade.php
+++ resources/views/home/live.blade.php
@@ -1,17 +1,15 @@
 @extends('app')
 
 @section('content')
-		<div class="content">
-			<div class="matchbox">
-				<div class="team1">
-					<img height="75px" src="{{ asset('images/teams/virtus.pro.png') }}" />
-					<div class="team-name">Virtus.pro</div>
-				</div>
-				<div class="team2">
-					<div class="team-name">Cloud 9</div>
-					<img height="75px" src="{{ asset('images/teams/cloud_9.png') }}" />
-				</div>
-			</div>
-			<div class="streambox"></div>
+	<div class="matchbox">
+		<div class="team1">
+			<img height="75px" src="{{ asset('images/teams/virtus.pro.png') }}" />
+			<div class="team-name">Virtus.pro</div>
 		</div>
+		<div class="team2">
+			<div class="team-name">Cloud 9</div>
+			<img height="75px" src="{{ asset('images/teams/cloud_9.png') }}" />
+		</div>
+	</div>
+	<div class="streambox"></div>
 @endsection
