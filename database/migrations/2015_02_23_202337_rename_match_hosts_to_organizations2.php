92894fb5f4aa0efc7734eb03c822040e6593ed82
Michal Hojgr <michal.hojgr@gmail.com>
2015-02-25 09:51:47 +0100

    composer: fix

--- database/migrations/2015_02_23_202337_rename_match_hosts_to_organizations2.php
+++ database/migrations/2015_02_23_202337_rename_match_hosts_to_organizations2.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RenameMatchHostsToOrganizations extends Migration {
+class RenameMatchHostsToOrganizations2 extends Migration {
 
 	/**
 	 * Run the migrations.
