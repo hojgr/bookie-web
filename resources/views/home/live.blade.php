fee0c3c95273d35ef5886237d214b6ce91c7657f
Robert Danielson <rob@seligtech.com>
2015-02-25 09:53:09 -0500

    few more design placeholders

--- resources/views/home/live.blade.php
+++ resources/views/home/live.blade.php
@@ -20,6 +20,10 @@
 @endsection
 
 @section('rightside')
+    <div class="bet-locked">
+        <h1>Bet Locked</h1>
+        <h3>Total Value: $5.93</h3>
+    </div>
     <div class="bet">
         <h3>Your bet:</h3>
         <div class="itembox">
@@ -85,118 +89,119 @@
 
     <div class="inventory">
         <div class="bet">
-                <h3>Your bet:</h3>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
+            <h3>Your inventory:</h3>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                <div class="price">$0.00</div>
+                <div class="image">
+                    <img src="https://steamcommunity-a.akamaihd.net/economy/image/fWFc82js0fmoRAP-qOIPu5THSWqfSmTELLqcUywGkijVjZYMUrsm1j-9xgEObwgfEh_nvjlWhNzZCveCDfIBj98xqodQ2CZknz58OOy2OwhkZzvFDa9dV7g2_Rn5DDQx7cl3a9u_8LMSZw3qtdfPNrR5ZNwdSpbSUqXVNQz4vktr0aUPK5bYp37m2yntbG0MCkD1ujVTR3uA_-U/150fx100f" />
+                </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
                     <div class="price">$0.00</div>
                     <div class="image">
                         <img src="https://steamcommunity-a.akamaihd.net/economy/image/fWFc82js0fmoRAP-qOIPu5THSWqfSmTELLqcUywGkijVjZYMUrsm1j-9xgEObwgfEh_nvjlWhNzZCveCDfIBj98xqodQ2CZknz58OOy2OwhkZzvFDa9dV7g2_Rn5DDQx7cl3a9u_8LMSZw3qtdfPNrR5ZNwdSpbSUqXVNQz4vktr0aUPK5bYp37m2yntbG0MCkD1ujVTR3uA_-U/150fx100f" />
                     </div>
                     <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/fWFc82js0fmoRAP-qOIPu5THSWqfSmTELLqcUywGkijVjZYMUrsm1j-9xgEObwgfEh_nvjlWhNzZCveCDfIBj98xqodQ2CZknz58OOy2OwhkZzvFDa9dV7g2_Rn5DDQx7cl3a9u_8LMSZw3qtdfPNrR5ZNwdSpbSUqXVNQz4vktr0aUPK5bYp37m2yntbG0MCkD1ujVTR3uA_-U/150fx100f" />
-                        </div>
-                        <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
-                <div class="itembox">
-                    <div class="stattrak">ST</div>
-                        <div class="price">$0.00</div>
-                        <div class="image">
-                            <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
-                        </div>
-                    <div class="wear mw">Minimum Wear</div>
-                </div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+            <div class="itembox">
+                <div class="stattrak">ST</div>
+                    <div class="price">$0.00</div>
+                    <div class="image">
+                        <img src="https://steamcommunity-a.akamaihd.net/economy/image/Oopn2-pcvWM3ClBZPQNmE4LGrWquLLMj48SDQ9nlLrTzgVAgs0D7cZfTTKcbYzqdmsH2Laczsyflz4RR2PM5vvzDRCqiRPx4h6JIrRgpKYye170142b9PunOtUOcom2O8Zl_Lq8RvkuQkk26FxM2iI7brHe6YvgqvsLYEc_zaLSljxJ79BKxJZDES_0ULT-L2YTof6YypHizwokXlrgqv_U=/150fx100f" />
+                    </div>
+                <div class="wear mw">Minimum Wear</div>
+            </div>
+        </div>
     </div>
 @endsection
