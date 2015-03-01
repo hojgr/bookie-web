b04af3dd5f2b5cd2b2f8c9c9fd55850009975a60
Michal Hojgr <michal.hojgr@gmail.com>
2015-03-01 04:21:20 +0100

    article: add form

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Bookie.GG - Upcoming Matches</title>
 
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
 	<link href="{{ elixir('css/admin.css') }}" rel="stylesheet">
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,100,900' rel='stylesheet' type='text/css'>
 
--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -2,14 +2,27 @@
 
 @section('content')
 	<div class="content-box">
+		<script src="//cdn.ckeditor.com/4.4.7/standard/ckeditor.js"></script>
+
 		<h2>Create new article!</h2>
-		<input disabled value="soon" />
+
+		{!! Form::open(['route' => 'admin.article.store']) !!}
+		{!! Form::text('title', null, array('placeholder' => 'Title of article', 'class' => 'form-control')) !!}<br />
+		{!! Form::textarea('text', null, array('placeholder' => 'Text of article')) !!}<br />
+
+		{!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+		{!! Form::close() !!}
+		<script>
+			// Replace the <textarea id="editor1"> with a CKEditor
+			// instance, using default configuration.
+			CKEDITOR.replace('text');
+		</script>
 	</div>
 @endsection
 
 
 @section('rightside')
 	<div class="right-side">
-		<h3 style="margin-left: 10px">TODO: stats</h3>
+		<h3 style="margin-left: 10px">TODO: articles</h3>
 	</div>
 @endsection
