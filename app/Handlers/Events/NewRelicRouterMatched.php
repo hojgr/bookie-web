39e4a17d06b144bc9ce67be3374d83f8e39887f9
Michal Hojgr <michal.hojgr@gmail.com>
2015-03-06 15:36:46 +0100

    newrelic: add route name as transaction name

---
+++ app/Handlers/Events/NewRelicRouterMatched.php
@@ -0,0 +1,36 @@
+<?php namespace BookieGG\Handlers\Events;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldBeQueued;
+use Illuminate\Routing\Route;
+
+class NewRelicRouterMatched {
+
+	/**
+	 * Create the event handler.
+	 *
+	 */
+	public function __construct()
+	{
+		//
+	}
+
+	/**
+	 * Handle the event.
+	 *
+	 * @param Route $route
+	 * @param Request $request
+	 * @internal param $router .matched  $event
+	 */
+	public function handle(Route $route, Request $request)
+	{
+		$name = $route->getActionName();
+
+		$name = preg_replace('/BookieGG\\\\Http\\\\Controllers\\\\/', '', $name);
+
+		newrelic_name_transaction($name);
+	}
+
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -11,8 +11,8 @@ class EventServiceProvider extends ServiceProvider {
 	 * @var array
 	 */
 	protected $listen = [
-		'event.name' => [
-			'EventListener',
+		'router.matched' => [
+			'BookieGG\Handlers\Events\NewRelicRouterMatched',
 		],
 	];
 
