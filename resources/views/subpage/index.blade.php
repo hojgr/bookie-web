f92ce454177361e4ff32b866cfa8d1ff79884252
birjolaxew <johanringmann@gmail.com>
2015-03-28 22:06:15 +0100

    Styled subpages

--- resources/views/subpage/index.blade.php
+++ resources/views/subpage/index.blade.php
@@ -1,7 +1,12 @@
 @extends('app')
 
 @section('content')
-	<div class="content-box {{ strtolower(preg_replace('/ /', '-', $data->name)) }}">
-		{!! $data->content !!}
+<div class="column medium-column">
+	<div class="module sub-module">
+		<h1>{{ $data->name }}</h1>
+		<div class="module-content">
+			{!! $data->content !!}
+		</div>
 	</div>
+</div>
 @endsection
--- resources/views/subpage/partners.blade.php
+++ resources/views/subpage/partners.blade.php
@@ -1,8 +1,9 @@
 @extends('app')
 
 @section('content')
-	<a href="http://opskins.com" target="_blank" class="noblue">
-		<div class="content-box" style="text-align: justify; float: left">
+<div class="column medium-column">
+	<div class="module partner-module">
+		<a href="http://opskins.com" target="_blank" class="noblue">
 			<img style="width: 620px; height: auto; position: relative; left: -10px; top: -10px" src="http://img3.fotos-hochladen.net/uploads/ckras2awgfk1536itgpfmh2u4.png" />
 			<p>A secured marketplace allowing you to exchange your CSGO goodies for real life money.</p>
 
@@ -12,6 +13,7 @@
 				but after months of empty promises, we have decided to fill the gap ourselves.
 				The team behind OPSkins is passionate and composed of experienced entrepreneurs,
 				UI designers and developers. Please head over to the help section to see how it all works.</p>
-		</div>
-	</a>
+		</a>
+	</div>
+</div>
 @endsection
