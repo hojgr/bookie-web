fb15ca4e6f9aba4545171c07f9ca8cb612ab748a
birjolaxew <johanringmann@gmail.com>
2015-03-28 20:54:19 +0100

    Added favicon to landing page

--- resources/views/beta/landing.blade.php
+++ resources/views/beta/landing.blade.php
@@ -4,6 +4,20 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="/favicon/favicon-32x32.png?v=3" sizes="32x32">
+	<link rel="icon" type="image/png" href="/favicon/favicon-96x96.png?v=3" sizes="96x96">
+	<link rel="icon" type="image/png" href="/favicon/favicon-16x16.png?v=3" sizes="16x16">
+	<link rel="manifest" href="/favicon/manifest.json">
+	
 	<title>Bookie.GG Beta Sign-up</title>
 
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,100,900' rel='stylesheet' type='text/css'>
@@ -144,55 +158,7 @@
 				{!! Form::close() !!}
 			@endif
 		@endif
-
-
-		<!--<div class="content">
-			<h1 class="center">Bookie.GG</h1>
-			<h1>Coming Soon
-				<span class="fix">&nbsp;</span></h1>
-			<h2>A Revolution for Esports Betting
-				<span class="fix">&nbsp;</span></h2>
-			<img class="logo" src="{{ asset('images/logo_beta.png') }}" />
-			@if (!Auth::user() || Auth::user()->subscription === null)
-				<h2>Sign Up for Updates and Beta Access
-				<span class="fix">&nbsp;</span></h2>
-			@endif
-		@if (!Auth::check())
-				<a href="{{ action("SteamController@login") }}" class="connect"><img src="{{ asset('images/steamsignin.png') }}"/></a>
-			@else
-				<div class="userbox" style="position: relative">
-					<img src="{{ SteamUtil::avatarPathToAvatarURL(Auth::user()->avatar_path) }}">
-					<div class="right">
-						<span>Logged in as:</span>
-						<h2>{{ Auth::user()->display_name }}</h2>
-						<a style="color: white; position: absolute; top: 0; right: 0; padding: 20px" href="{{ route('logout') }}">Logout</a>
-					</div>
-					<div class="clr"></div>
-				</div>
-				@if (Auth::user()->subscription !== null)
-					<div class="signed_up">
-						<h3>You have signed up for closed beta.</h3>
-						When you account is activated you will receive an e-mail informing you so.
-					</div>
-				@else
-					@foreach($errors->all() as $error)
-						<span class="error" style="color: white">{{ $error }}</span>
-					@endforeach
-
-					!! Form::open(['route' => 'subscribe']) !!}
-					{!! Form::text('email', null, array('placeholder' => 'Your Email')) !!}
-					{!! Form::text('name', null, array('placeholder' => 'Your Name')) !!}
-
-					{!! Form::submit('Sign Up') !!}
-					{!! Form::close() !!}-->
-				@endif
-			@endif
-		</div>
 	</div>
-
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
 
