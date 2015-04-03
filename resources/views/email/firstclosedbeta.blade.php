b4a001a7449d13a22f30838bd0a160fd3b72a086
Michal Hojgr <michal.hojgr@gmail.com>
2015-04-03 18:52:10 +0200

    email: edit first email

--- resources/views/email/firstclosedbeta.blade.php
+++ resources/views/email/firstclosedbeta.blade.php
@@ -1,89 +1,111 @@
-<html>
-<head>
-	<title>Untitled-1</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body bgcolor="#FFFFFF" leftmargin="10" topmargin="10" marginwidth="10" marginheight="10">
-<!-- Save for Web Slices (Untitled-1) -->
-<table id="Table_01" width="801" height="1002" border="0" cellpadding="0" cellspacing="0">
-	<tr>
-		<td colspan="3">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_01.png') }}" width="291" height="93" alt=""></td>
-		<td colspan="3"><a href="bookie.gg"><img src="{{ asset('images/email/firstclosedbeta/email2_02.png') }}" width="215" height="93" alt=""></a></td>
-		<td colspan="3">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_03.png') }}" width="294" height="93" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="1" height="93" alt=""></td>
-	</tr>
-	<tr>
-		<td height="2">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_04.png') }}" width="9" height="1" alt=""></td>
-		<td colspan="7" rowspan="2"><p> Thank you for  signing up for closed beta access to Bookie.GG</p>
-			<p>From all of us on  the Bookie.GG staff, we'd like to welcome you, and once again thank you for  helping us test our website.            </p>
-			<p>In a few more  weeks Bookie.GG will officially launch. However, you won't need to wait a few  more weeks, you have been among the few selected to get a sneak peak at  Bookie.GG. Based off user feedback, such as your own, we will continue to  improve our website to become the best CS:GO betting platform out there.            </p>
-			<p>During our closed  beta users will be able to deposit/withdraw items from our Bank feature, place  bets on their favorite CS:GO teams, use the website on their mobile devices,  and provide us with feedback at www.reddit.com/r/BookieGG to ensure all  necessary changes are made.            </p>
-			<p>To get your beta  access;<br>
-				1. Go to  www.Bookie.GG<br>
-				2. Sign in with  your Steam account<br>
-				3. Enter this  code: [UNIQUE CODE]<br>
-				4. Enjoy the  website            </p>
-			<p>You can read our  Getting Started guide at www.bookie.gg/help - If you run into any issues using  our website, feel free to contact us at our official reddit at  www.reddit.com/r/BookieGG or you may use our contact form at  www.bookie.gg/contact (alternatively you can email our support department at  support@bookie.gg)            </p>
-			<p>Thanks,                   </p>
-			<p>The Bookie.GG  Staff</p></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/email2_06.png') }}" width="9" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="1" height="1" alt=""></td>
-	</tr>
-	<tr>
-		<td rowspan="3">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_07.png') }}" width="9" height="906" alt=""></td>
-		<td rowspan="3">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_08.png') }}" width="9" height="906" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="1" height="798" alt=""></td>
-	</tr>
-	<tr>
-		<td rowspan="2"><img src="{{ asset('images/email/firstclosedbeta/email2_09.png') }}" width="273" height="108" alt=""></td>
-		<td colspan="2">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_10.png') }}" width="77" height="73" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/email2_11.png') }}" width="77" height="73" alt=""></td>
-		<td colspan="2">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_12.png') }}" width="79" height="73" alt=""></td>
-		<td rowspan="2">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_13.png') }}" width="276" height="108" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="1" height="73" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="5">
-			<img src="{{ asset('images/email/firstclosedbeta/email2_14.png') }}" width="233" height="35" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="1" height="35" alt=""></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="9" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="273" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="9" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="68" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="77" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="70" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="9" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="276" height="1" alt=""></td>
-		<td>
-			<img src="{{ asset('images/email/firstclosedbeta/spacer.gif') }}" width="9" height="1" alt=""></td>
-		<td></td>
-	</tr>
-</table>
-<!-- End Save for Web Slices -->
-</body>
-</html>
+<center class="wrapper" style="display: table;table-layout: fixed;width: 100%;min-width: 620px;-webkit-text-size-adjust: 100%;-ms-text-size-adjust: 100%;background-color: #02a4d9;font-family: Roboto, sans-serif;">
+	<table class="gmail" style="border-collapse: collapse;border-spacing: 0;width: 650px;min-width: 650px">
+		<tbody>
+		<tr>
+			<td style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px">&nbsp;</td>
+		</tr>
+		</tbody>
+	</table>
+	<table class="header centered" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 602px">
+		<tbody>
+		<tr>
+			<td class="logo" style="padding: 16px 0 32px;vertical-align: top;mso-line-height-rule: at-least">
+				<div class="logo-center" style="font-size: 26px;font-weight: 700;letter-spacing: -0.02em;line-height: 32px;color: #41637e;text-align: center" align="center" id="emb-email-header">
+					<a href="http://www.bookie.gg"><img style="border: 0;-ms-interpolation-mode: bicubic;display: block;Margin-left: auto;Margin-right: auto;max-width: 360px" src="http://bookie.gg/images/header.png" alt="Bookie.GG" width="300" height="59" /></a>
+				</div>
+			</td>
+		</tr>
+		</tbody>
+	</table>
+	<table class="border" style="border-collapse: collapse;border-spacing: 0;font-size: 1px;line-height: 1px;background-color: #0299ca;Margin-left: auto;Margin-right: auto" width="602">
+		<tbody>
+		<tr>
+			<td style="padding: 0;vertical-align: top">&#8203;</td>
+		</tr>
+		</tbody>
+	</table>
+	<table class="centered" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto">
+		<tbody>
+		<tr>
+			<td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #0299ca;width: 1px">&#8203;</td>
+			<td style="padding: 0;vertical-align: top">
+				<table class="one-col" style="border-collapse: collapse;border-spacing: 0;Margin-left: auto;Margin-right: auto;width: 600px;background-color: #ffffff;font-size: 14px;table-layout: fixed">
+					<tbody>
+					<tr>
+						<td class="column" style="padding: 0;vertical-align: top;text-align: left">
+							<div>
+								<div class="column-top" style="font-size: 32px;line-height: 32px">&nbsp;</div>
+							</div>
+							<table class="contents" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%">
+								<tbody>
+								<tr>
+									<td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px;word-break: break-word;word-wrap: break-word">
+										<h1 style="Margin-top: 0;color: #565656;font-weight: 700;font-size: 36px;Margin-bottom: 18px;line-height: 44px">Welcome!</h1>
+										<p style="Margin-top: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-bottom: 25px">You've been accepted into the Bookie.GG closed beta, giving you access to the site prior to release.</p>
+										<p style="Margin-top: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-bottom: 25px">Your activation code is: <span style="Margin-top: 0;color: #02a4d9;font-weight: normal;font-size: 18px;line-height: 26px;Margin-bottom: 16px;Margin-left: 4px;padding: 4px 8px;background: #fafafa;border:1px solid #dcdcdc;border-radius:3px">ABC123XYZ</span>
+										</p>
+										<p style="Margin-top: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-bottom: 25px">From all of us on the Bookie.GG staff, we'd like to welcome you and once again thank you for helping us test our website.</p>
+										<p style="Margin-top: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-bottom: 25px">To activate your beta access, please do the following:</p>
+										<ul style="Margin-top: 0;padding-left: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-left: 18px;Margin-bottom: 25px">
+											<li style="Margin-top: 0;padding-left: 0">Go to&nbsp;<a style="text-decoration: underline;transition: all .2s;color: #02a4d9" data-emb-href-display="www.bookie.gg" href="http://www.bookie.gg">www.bookie.gg</a>
+											</li>
+											<li style="Margin-top: 0;padding-left: 0">Sign in with your Steam account</li>
+											<li style="Margin-top: 0;padding-left: 0">Enter your activation code (<span style="color: #02a4d9">ABC123XYZ</span>)</li>
+											<li style="Margin-top: 0;padding-left: 0">Enjoy the site!</li>
+										</ul>
+									</td>
+								</tr>
+								</tbody>
+							</table>
+							<table class="contents" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%">
+								<tbody>
+								<tr>
+									<td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px;word-break: break-word;word-wrap: break-word">
+										<div class="btn" style="Margin-bottom: 24px;padding: 2px;text-align: center">
+											<!--[if !mso]--><a style="display: inline-block;font-size: 13px;font-weight: bold;line-height: 15px;outline: none;padding: 10px 30px;text-align: center;text-decoration: none !important;transition: all .2s;color: #fff !important;background-color: #02a4d9;outline-color: #02a4d9;border-radius:3px" href="http://www.bookie.gg">Let's go!</a>
+											<!--[endif]-->
+											<!--[if mso]>
+											<v:rect xmlns:v="urn:schemas-microsoft-com:vml" href="http://www.bookie.gg" style="width:110px" fillcolor="#02A4D9" strokecolor="#02A4D9" strokeweight="6px">
+												<v:stroke linestyle="thinthin"></v:stroke>
+												<v:textbox style="mso-fit-shape-to-text:t" inset="0px,7px,0px,7px">
+													<center style="font-size:13px;line-height:15px;color:#FFFFFF;font-family:Tahoma,sans-serif;font-weight:bold;mso-line-height-rule:exactly;mso-text-raise:0px">Let&#39;s go!</center>
+												</v:textbox>
+											</v:rect>
+											<![endif]-->
+										</div>
+									</td>
+								</tr>
+								</tbody>
+							</table>
+							<table class="contents" style="border-collapse: collapse;border-spacing: 0;table-layout: fixed;width: 100%">
+								<tbody>
+								<tr>
+									<td class="padded" style="padding: 0;vertical-align: top;padding-left: 32px;padding-right: 32px;word-break: break-word;word-wrap: break-word">
+										<p style="Margin-top: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-bottom: 25px">During closed beta, users will be able to deposit/withdraw items from our banking system, place bets on their favourite CS:GO teams, and provide important feedback, allowing us to improve the site before the release.</p>
+										<p style="Margin-top: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-bottom: 25px">You can read our getting started guide on&nbsp;<a style="text-decoration: underline;transition: all .2s;color: #02a4d9" data-emb-href-display="www.bookie.gg/help" href="http://www.bookie.gg/help">www.bookie.gg/help</a>&nbsp;- if you run into any issues using our site, feel free to contact us at our official subreddit&nbsp;<a style="text-decoration: underline;transition: all .2s;color: #02a4d9" data-emb-href-display="reddit.com/r/bookiegg" href="http://reddit.com/r/bookiegg">/r/bookieGG</a>, email our support department at&nbsp;<a style="text-decoration: underline;transition: all .2s;color: #02a4d9" data-emb-href-display="support@bookie.gg" href="mailto:support@bookie.gg">support@bookie.gg</a>&nbsp;or use the built-in contact form at&nbsp;<a style="text-decoration: underline;transition: all .2s;color: #02a4d9" data-emb-href-display="www.bookie.gg/contact" href="http://www.bookie.gg/contact">www.bookie.gg/contact</a>
+										</p>
+										<p style="Margin-top: 0;color: #565656;font-size: 16px;line-height: 25px;Margin-bottom: 24px">Thanks,
+											<br>- The Bookie.GG staff</p>
+									</td>
+								</tr>
+								</tbody>
+							</table>
+							<div class="column-bottom" style="font-size: 8px;line-height: 8px">&nbsp;</div>
+						</td>
+					</tr>
+					</tbody>
+				</table>
+			</td>
+			<td class="border" style="padding: 0;vertical-align: top;font-size: 1px;line-height: 1px;background-color: #0299ca;width: 1px">&#8203;</td>
+		</tr>
+		</tbody>
+	</table>
+	<table class="border" style="border-collapse: collapse;border-spacing: 0;font-size: 1px;line-height: 1px;background-color: #0299ca;Margin-left: auto;Margin-right: auto" width="602">
+		<tbody>
+		<tr>
+			<td style="padding: 0;vertical-align: top">&#8203;</td>
+		</tr>
+		</tbody>
+	</table>
+	<div class="spacer" style="font-size: 1px;line-height: 32px;width: 100%">&nbsp;</div>
+</center>
