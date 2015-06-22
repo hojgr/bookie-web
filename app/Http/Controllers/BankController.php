fd8fdb58bf9d1e500f1f5584bd62b56938195a71
Michal Hojgr <michal.hojgr@gmail.com>
2015-06-22 17:30:19 +0200

    style: fix

--- app/Http/Controllers/BankController.php
+++ app/Http/Controllers/BankController.php
@@ -10,10 +10,7 @@ use BookieGG\Contracts\BankLoaderInterface;
 use BookieGG\Services\ItemUtility;
 
 class BankController extends Controller {
-    public function show(
-        InventoryLoaderInterface $inventoryLoader,
-        BankLoaderInterface $bankLoader
-    ) {
+    public function show(InventoryLoaderInterface $inventoryLoader, BankLoaderInterface $bankLoader) {
 		return view("bank/bank")
 			->with('userInventory', $inventoryLoader->loadSteamInventory(\Auth::getUser()->steam_id))
 			->with('userBank', $bankLoader->load(\Auth::getUser()));
