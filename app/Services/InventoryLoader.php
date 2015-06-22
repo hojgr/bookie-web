36c53377e8f5948cb50d60c3cec586cbd3385bef
Michal Hojgr <michal.hojgr@gmail.com>
2015-06-22 20:23:17 +0200

    style: convert to PSR2

--- app/Services/InventoryLoader.php
+++ app/Services/InventoryLoader.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Inventory Loader
+ *
+ * PHP version 5.6
+ *
+ * @category Class
+ * @package  BookieGG\Services
+ * @author   Michal Hojgr <michal.hojgr@gmail.com>
+ * @license  MS Reference
+ * @link     http://bookie.gg
+ */
 
 namespace BookieGG\Services;
 
@@ -6,44 +17,96 @@ use BookieGG\Contracts\ItemUtilityContract;
 use BookieGG\Models\CsgoItem;
 use BookieGG\Contracts\InventoryLoaderInterface;
 
-class InventoryLoader implements InventoryLoaderInterface {
+/**
+ * InventoryLoader
+ *
+ * @category Class
+ * @package  BookieGG\Services
+ * @author   Michal Hojgr <michal.hojgr@gmail.com>
+ * @license  MS Reference
+ * @link     http://bookie.gg
+ */
+class InventoryLoader implements InventoryLoaderInterface
+{
 
-    /** @var ItemUtilityContract */
+    /**
+     * Item utility
+     *
+     * @var ItemUtilityContract item utility
+     */
     protected $itemUtility;
 
-    public function __construct(ItemUtilityContract $itemUtil) {
+    /**
+     * Constructor
+     *
+     * @param ItemUtilityContract $itemUtil contract
+     */
+    public function __construct(ItemUtilityContract $itemUtil)
+    {
         $this->itemUtility = $itemUtil;
     }
 
-    public function loadSteamInventoryJSON($steamId64) {
-        $inventoryJsonURL = "http://steamcommunity.com/profiles/" . $steamId64 . "/inventory/json/730/2";
+    /**
+     * Loads JSON of given id
+     *
+     * @param string $steamId64 Id of user to be loaded
+     *
+     * @return json Json of steamid's inventory
+     */
+    public function loadSteamInventoryJSON($steamId64)
+    {
+        $inventoryJsonURL = "http://steamcommunity.com/profiles/"
+            . $steamId64
+            . "/inventory/json/730/2";
+
         $inventoryJsonString = file_get_contents($inventoryJsonURL);
         $inventoryJson = json_decode($inventoryJsonString);
 
         return $inventoryJson;
     }
 
-    public function loadSteamInventory($steamId64) {
+    /**
+     * Loads user's inventory into an frontend object
+     *
+     * @param string $steamId64 Id of user to be loaded
+     *
+     * @return object Object
+     */
+    public function loadSteamInventory($steamId64)
+    {
         $inventoryJson = $this->loadSteamInventoryJSON($steamId64);
 
         $inv = [];
         $names = [];
-        foreach($inventoryJson->rgInventory as $item) {
+        foreach ($inventoryJson->rgInventory as $item) {
+            $jsonKey = $item->classid . "_" . $item->instanceid;
+            $itemDescription = $inventoryJson->rgDescriptions->$jsonKey;
 
-            $itemDescription = $inventoryJson->rgDescriptions->{$item->classid . "_" . $item->instanceid};
-
-            if(!$itemDescription->tradable)
+            if (!$itemDescription->tradable) {
                 continue;
+            }
 
             $names[] = $itemDescription->market_hash_name;
+
+            $exterior = $this->itemUtility->getExterior(
+                $itemDescription->market_hash_name
+            );
+
+            $isStattrak = str_contains(
+                $itemDescription->market_hash_name,
+                "StatTrak"
+            );
+
             $inv[] = (object) [
-                "id" => $item->id, // this is used as index for all POST params - must be unique for inventory
+                "id" => $item->id, // this is used as index for all POST params
                 "weaponName" => $itemDescription->market_hash_name,
-                "exterior" => $this->itemUtility->getExterior($itemDescription->market_hash_name),
+                "exterior" => $exterior,
                 "quality" =>  $itemDescription->type,
                 "price" => "??",
-                "stattrak" => str_contains($itemDescription->market_hash_name, "StatTrak"),
-                "image" => "http://steamcommunity-a.akamaihd.net/economy/image/" . $itemDescription->icon_url . "/90fx60f",
+                "stattrak" => $isStattrak,
+                "image" => "http://steamcommunity-a.akamaihd.net/economy/image/"
+                    . $itemDescription->icon_url
+                . "/90fx60f",
                 "steam_info" => [
                     "id" => $item->id,
                     "class_id" => $item->classid,
@@ -52,24 +115,33 @@ class InventoryLoader implements InventoryLoaderInterface {
             ];
         }
 
-        $prices = CsgoItem::whereIn('market_name', $names)->get()->keyBy("market_name");
+        $prices = CsgoItem::whereIn('market_name', $names)
+            ->get()
+            ->keyBy("market_name");
 
-        foreach($inv as $id => $item) {
-            if(!isset($prices[$item->weaponName])) {
+        foreach ($inv as $id => $item) {
+            if (!isset($prices[$item->weaponName])) {
                 unset($inv[$id]);
-            } else {
-                $item->price = $prices[$item->weaponName]->latestPrice->price;
+                continue;
             }
+
+            $item->price = $prices[$item->weaponName]->latestPrice->price;
         }
 
-        usort($inv, function($a,$b) {
-            if ($a->price == $b->price) { return 0; }
+        usort(
+            $inv,
+            function ($a, $b) {
+                if ($a->price == $b->price) {
+                    return 0;
+                }
 
-            if(floatval($b->price) > floatval($a->price))
-                return 1;
+                if (floatval($b->price) > floatval($a->price)) {
+                    return 1;
+                }
 
-            return -1;
-        });
+                return -1;
+            }
+        );
 
         return $inv;
     }
