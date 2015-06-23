b0c537c194beb723a8b79d34e6274ee7fc85e708
Michal Hojgr <michal.hojgr@gmail.com>
2015-06-23 16:56:19 +0200

    code style: add doc and reformat long lines

--- resources/views/bank/bank.blade.php
+++ resources/views/bank/bank.blade.php
@@ -1,3 +1,14 @@
+/**
+ * A view for main bank page
+ *
+ * PHP version 5.6
+ *
+ * @category View
+ * @package  Bank
+ * @author   Johan Fagerberg <now@known.com>
+ * @license  MS Reference
+ * @link     http://bookie.gg
+ */
 {{-- */ $wideLayout=true; /*--}}
 @extends('app')
 
@@ -5,13 +16,29 @@
 <div class="column small-medium">
 	<div class="module bank-module">
 		<h2>My Bank</h2>
-		@include('partials/inventory', ['submitUrl' => '/api/csgo/bank/withdraw', 'items' => $userBank, 'btns' => ['Withdraw' => ''], 'emptyText' => 'Select items to withdraw'])
+        @include(
+            'partials/inventory',
+            [
+                'submitUrl' => '/api/csgo/bank/withdraw',
+                'items' => $userBank,
+                'btns' => ['Withdraw' => ''],
+                'emptyText' => 'Select items to withdraw'
+            ]
+        )
 	</div>
 </div>
 <div class="column small-medium">
 	<div class="module inventory-module">
 		<h2>My Inventory</h2>
-		@include('partials/inventory', ['submitUrl' => '/api/csgo/bank/deposit', 'items' => $userInventory, 'btns' => ['Deposit' => ''], 'emptyText' => 'Select items to deposit'])
+        @include(
+            'partials/inventory', 
+            [
+                'submitUrl' => '/api/csgo/bank/deposit',
+                'items' => $userInventory,
+                'btns' => ['Deposit' => ''],
+                'emptyText' => 'Select items to deposit'
+            ]
+        )
 	</div>
 </div>
 @endsection
