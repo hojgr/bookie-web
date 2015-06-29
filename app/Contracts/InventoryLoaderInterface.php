a453a7dac5b316bb3012191ca29dfc6c43c37dee
Michal Hojgr <michal.hojgr@gmail.com>
2015-06-29 18:50:36 +0200

    inventory: prepare for invalidation implementation

--- app/Contracts/InventoryLoaderInterface.php
+++ app/Contracts/InventoryLoaderInterface.php
@@ -9,4 +9,16 @@ interface InventoryLoaderInterface
      * on steam page
      */
     public function getSteamInventory($steamId64);
+
+    /**
+     * Invalidate cache of given steamId
+     *
+     * Made to invalidate cache after known inventory manipulation
+     * such as finished trade etc.
+     *
+     * @param string $steamId64 Steam ID
+     *
+     * @return void
+     */
+    public function invalidateCache($steamId64);
 }
--- app/Services/InventoryLoader.php
+++ app/Services/InventoryLoader.php
@@ -71,6 +71,21 @@ class InventoryLoader implements InventoryLoaderInterface
     }
 
     /**
+     * Invalidate cache of given steamId
+     *
+     * Made to invalidate cache after known inventory manipulation
+     * such as finished trade etc.
+     *
+     * @param string $steamId64 Steam ID
+     *
+     * @return void
+     */
+    public function invalidateCache($steamId64)
+    {
+        
+    }
+
+    /**
      * Loads JSON of given id
      *
      * @param string $steamId64 Id of user to be loaded
