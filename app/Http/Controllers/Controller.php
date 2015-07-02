7627cca420f7a704412cbe0613ca47b2405fff3d
Michal Hojgr <michal.hojgr@gmail.com>
2015-07-02 23:03:24 +0200

    popup: add $isPopupActive variable to views
    indicating if any popup is active

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,15 +4,20 @@ use BookieGG\Contracts\Repositories\ArticleRepositoryInterface;
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use BookieGG\Services\PopupManager;
 
 abstract class Controller extends BaseController {
 
     use DispatchesCommands, ValidatesRequests;
 
-    public function __construct(ArticleRepositoryInterface $ari) {
+    public function __construct(
+        ArticleRepositoryInterface $ari,
+        PopupManager $popupManager
+    ) {
         // TODO: move to separate class ( http://laravel.com/docs/5.0/views#view-composers )
-        \View::composer('app', function($v) use ($ari) {
+        \View::composer('app', function($v) use ($ari, $popupManager) {
             $v->with('articles', $ari->all());
+            $v->with('isPopupActive', $popupManager->hasActivePopup(auth()->getUser()));
         });
     }
 }
---
+++ app/Services/PopupManager.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Handles popups
+ *
+ * PHP version 5.6
+ *
+ * @category Class
+ * @package  BookieGG\Services
+ * @author   Michal Hojgr <michal.hojgr@gmail.com>
+ * @license  MS Reference
+ * @link     http://bookie.gg
+ */
+
+namespace BookieGG\Services;
+
+use BookieGG\Models\User;
+use BookieGG\Services\TradeManager;
+
+class PopupManager
+{
+    public function hasActivePopup(User $user)
+    {
+        $userTrade = $user->user_last_trade;
+
+        if ($userTrade->status === TradeManager::STATUS_ACTIVE) {
+            return true;
+        }
+
+        if ($userTrade->status === TradeManager::STATUS_QUEUE) {
+            return true;
+        }
+    }
+}
